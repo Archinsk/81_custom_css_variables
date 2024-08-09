85d69b7e8402f076cc98c66defb0888ad0193ead
Mihail Vasilev <mgp-nsk@yandex.ru>
2024-08-09 18:13:58 +0700

    start template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,178 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="header">
+        <div class="header-text">Header</div>
+        <div class="buttons-group">
+          <div class="button">A</div>
+          <div class="button">B</div>
+          <div class="button">C</div>
+        </div>
+      </div>
+      <div class="main">
+        <div class="sidebar">
+          <div class="sidebar-item active">
+            <div class="sidebar-item-text">Sidebar-item-01</div>
+            <div class="buttons-group">
+              <div class="button">A</div>
+              <div class="button">B</div>
+              <div class="button">C</div>
+            </div>
+          </div>
+          <div class="sidebar-item">
+            <div class="sidebar-item-text">Sidebar-item-02</div>
+            <div class="buttons-group">
+              <div class="button">A</div>
+              <div class="button">B</div>
+              <div class="button">C</div>
+            </div>
+          </div>
+          <div class="sidebar-item">
+            <div class="sidebar-item-text">Sidebar-item-03</div>
+            <div class="buttons-group">
+              <div class="button">A</div>
+              <div class="button">B</div>
+              <div class="button">C</div>
+            </div>
+          </div>
+          <div class="sidebar-item">
+            <div class="sidebar-item-text">Sidebar-item-04</div>
+            <div class="buttons-group">
+              <div class="button">A</div>
+              <div class="button">B</div>
+              <div class="button">C</div>
+            </div>
+          </div>
+        </div>
+        <div class="card-list">
+          <div class="card active">
+            <div class="card-header">
+              <div class="item-property">
+                <div class="item-property-name">Header-1</div>
+                <div class="item-property-value">Subheader-1</div>
+              </div>
+              <div class="buttons-group">
+                <div class="button">A</div>
+                <div class="button">B</div>
+                <div class="button">C</div>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="item-property">
+                <div class="item-property-name">Property-1</div>
+                <div class="item-property-value">Value-1</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-2</div>
+                <div class="item-property-value">Value-2</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-3</div>
+                <div class="item-property-value">Value-3</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-4</div>
+                <div class="item-property-value">Value-4</div>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header">
+              <div class="item-property">
+                <div class="item-property-name">Header-1</div>
+                <div class="item-property-value">Subheader-1</div>
+              </div>
+              <div class="buttons-group">
+                <div class="button">A</div>
+                <div class="button">B</div>
+                <div class="button">C</div>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="item-property">
+                <div class="item-property-name">Property-1</div>
+                <div class="item-property-value">Value-1</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-2</div>
+                <div class="item-property-value">Value-2</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-3</div>
+                <div class="item-property-value">Value-3</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-4</div>
+                <div class="item-property-value">Value-4</div>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header">
+              <div class="item-property">
+                <div class="item-property-name">Header-1</div>
+                <div class="item-property-value">Subheader-1</div>
+              </div>
+              <div class="buttons-group">
+                <div class="button">A</div>
+                <div class="button">B</div>
+                <div class="button">C</div>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="item-property">
+                <div class="item-property-name">Property-1</div>
+                <div class="item-property-value">Value-1</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-2</div>
+                <div class="item-property-value">Value-2</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-3</div>
+                <div class="item-property-value">Value-3</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-4</div>
+                <div class="item-property-value">Value-4</div>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header">
+              <div class="item-property">
+                <div class="item-property-name">Header-1</div>
+                <div class="item-property-value">Subheader-1</div>
+              </div>
+              <div class="buttons-group">
+                <div class="button">A</div>
+                <div class="button">B</div>
+                <div class="button">C</div>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="item-property">
+                <div class="item-property-name">Property-1</div>
+                <div class="item-property-value">Value-1</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-2</div>
+                <div class="item-property-value">Value-2</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-3</div>
+                <div class="item-property-value">Value-3</div>
+              </div>
+              <div class="item-property">
+                <div class="item-property-name">Property-4</div>
+                <div class="item-property-value">Value-4</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import "./default-styles.scss";
+import "./neumorphism-theme.scss";
+import "./glassmorphism-theme.scss";
+import "./claymorphism-theme.scss";
+import "./retrofuturism-theme.scss";
+import "./hitech-theme.scss";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
